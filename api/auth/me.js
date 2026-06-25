ddfba32a20a9f7b38514f23c04ac2589409b3c3f
robertk-saffhire <robertk@saffhire.com>
2026-06-25 11:23:30 -0500

    Remove separate me API function

--- api/auth/me.js
+++
@@ -1,8 +0,0 @@
-import { json } from '../lib/db.js';
-import { getUserFromRequest, publicUser } from '../lib/auth.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return json(res, 405, { status: 'error', message: 'Method not allowed' });
-  const user = await getUserFromRequest(req);
-  return json(res, 200, { status: 'ok', user: publicUser(user) });
-}
